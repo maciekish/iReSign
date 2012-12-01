45824683cdb243ec8358538318d1ccfc992f7ce8
Esteban Bouza <esteban.bouza@gmail.com>
2012-12-01 23:15:30 +0100

    Added text field supporting drag and drop

---
+++ iReSign/IRTextFieldDrag.h
@@ -0,0 +1,13 @@
+//
+//  IRTextFieldDrag.h
+//  iReSign
+//
+//  Created by Esteban on 01/12/12.
+//  Copyright (c) 2012 nil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IRTextFieldDrag : NSTextField
+
+@end
---
+++ iReSign/IRTextFieldDrag.m
@@ -0,0 +1,13 @@
+//
+//  IRTextFieldDrag.m
+//  iReSign
+//
+//  Created by Esteban on 01/12/12.
+//  Copyright (c) 2012 nil. All rights reserved.
+//
+
+#import "IRTextFieldDrag.h"
+
+@implementation IRTextFieldDrag
+
+@end
